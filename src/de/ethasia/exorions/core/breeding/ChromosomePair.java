bbedb99f8fb2482a8f48e2e841b8dbc33a84c6e5
rondrejk <rondrejk@CE16848.corp.capgemini.com>
2019-01-25 17:50:48 +0100

    Implemented clone with crossover in ChromosomePair.

--- src/de/ethasia/exorions/core/breeding/ChromosomePair.java
+++ src/de/ethasia/exorions/core/breeding/ChromosomePair.java
@@ -1,6 +1,8 @@
 package de.ethasia.exorions.core.breeding;
 
 import de.ethasia.exorions.core.NotAllPropertiesAreSetException;
+import de.ethasia.exorions.core.interfaces.CoreClassesFactory;
+import de.ethasia.exorions.core.interfaces.RandomNumberGenerator;
 
 public class ChromosomePair {
     
@@ -30,19 +32,140 @@ public class ChromosomePair {
     //<editor-fold defaultstate="collapsed" desc="Methods">
     
     public ChromosomePair cloneWithCrossover() {
-        Chromosome clonedMaternalChromosome = maternalChromosome.createClone();
-        Chromosome clonedPaternalChromosome = paternalChromosome.createClone();
+        Chromosome.Builder firstDaughterChromosomeBuilder = new Chromosome.Builder();
+        Chromosome.Builder secondDaughterChromosomeBuilder = new Chromosome.Builder();
+        
+        setupChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+                
         ChromosomePair clonedPair = null;
         
         try {
             clonedPair = new ChromosomePair.Builder()
-                .setMaternalChromosome(clonedMaternalChromosome)
-                .setPaternalChromosome(clonedPaternalChromosome)
+                .setMaternalChromosome(firstDaughterChromosomeBuilder.build())
+                .setPaternalChromosome(secondDaughterChromosomeBuilder.build())
                 .build();
         } catch (NotAllPropertiesAreSetException ex) {
         }
         
         return clonedPair;
+    }
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Helper Methods">
+    
+    private void setupChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        setMaximumHealthAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setAccuracyAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setAttackAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setCriticalHitAvoidanceAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setCriticalHitFrequencyAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setDefenseAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setEvasivenessAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setSpecialAttackAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setSpecialDefenseAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);
+        setSwiftnessAlleleInCrossoverChildren(firstDaughterChromosomeBuilder, secondDaughterChromosomeBuilder);        
+    }
+    
+    private void setMaximumHealthAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getMaximumHealthAllele();
+        Allele paternalAllele = paternalChromosome.getMaximumHealthAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setMaximumHealthAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setMaximumHealthAllele(allelesAfterSwapping[1]);
+    }
+    
+    private void setAccuracyAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getAccuracyAllele();
+        Allele paternalAllele = paternalChromosome.getAccuracyAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setAccuracyAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setAccuracyAllele(allelesAfterSwapping[1]);
+    }
+    
+    private void setAttackAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getAttackAllele();
+        Allele paternalAllele = paternalChromosome.getAttackAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setAttackAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setAttackAllele(allelesAfterSwapping[1]);
+    }    
+    
+    private void setCriticalHitAvoidanceAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getCriticalHitAvoidanceAllele();
+        Allele paternalAllele = paternalChromosome.getCriticalHitAvoidanceAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setCriticalHitAvoidanceAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setCriticalHitAvoidanceAllele(allelesAfterSwapping[1]);
+    } 
+    
+    private void setCriticalHitFrequencyAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getCriticalHitFrequencyAllele();
+        Allele paternalAllele = paternalChromosome.getCriticalHitFrequencyAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setCriticalHitFrequencyAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setCriticalHitFrequencyAllele(allelesAfterSwapping[1]);
+    }
+    
+    private void setDefenseAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getDefenseAllele();
+        Allele paternalAllele = paternalChromosome.getDefenseAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setDefenseAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setDefenseAllele(allelesAfterSwapping[1]);
+    }
+
+    private void setEvasivenessAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getEvasivenessAllele();
+        Allele paternalAllele = paternalChromosome.getEvasivenessAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setEvasivenessAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setEvasivenessAllele(allelesAfterSwapping[1]);
+    }
+
+    private void setSpecialAttackAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getSpecialAttackAllele();
+        Allele paternalAllele = paternalChromosome.getSpecialAttackAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setSpecialAttackAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setSpecialAttackAllele(allelesAfterSwapping[1]);
+    }
+
+    private void setSpecialDefenseAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getSpecialDefenseAllele();
+        Allele paternalAllele = paternalChromosome.getSpecialDefenseAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setSpecialDefenseAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setSpecialDefenseAllele(allelesAfterSwapping[1]);
+    }   
+    
+    private void setSwiftnessAlleleInCrossoverChildren(Chromosome.Builder firstDaughterChromosomeBuilder, Chromosome.Builder secondDaughterChromosomeBuilder) {
+        Allele maternalAllele = maternalChromosome.getSwiftnessAllele();
+        Allele paternalAllele = paternalChromosome.getSwiftnessAllele();
+        
+        Allele[] allelesAfterSwapping = swapAllelesIfNecessary(maternalAllele, paternalAllele);
+        firstDaughterChromosomeBuilder.setSwiftnessAllele(allelesAfterSwapping[0]);
+        secondDaughterChromosomeBuilder.setSwiftnessAllele(allelesAfterSwapping[1]);
+    }     
+    
+    private Allele[] swapAllelesIfNecessary(Allele maternalAllele, Allele paternalAllele) {
+        RandomNumberGenerator rng = CoreClassesFactory.getInstance().getRandomNumberGeneratorSingletonInstance();
+        boolean swapAlleles = rng.createRandomBoolean();   
+        
+        if (swapAlleles) {
+            return new Allele[] {paternalAllele, maternalAllele};
+        }
+        
+        return new Allele[] {maternalAllele, paternalAllele};
     }
     
     //</editor-fold>
--- tests/de/ethasia/exorions/core/breeding/tests/ChromosomePairTest.java
+++ tests/de/ethasia/exorions/core/breeding/tests/ChromosomePairTest.java
@@ -1,6 +1,7 @@
 package de.ethasia.exorions.core.breeding.tests;
 
 import de.ethasia.exorions.core.NotAllPropertiesAreSetException;
+import de.ethasia.exorions.core.breeding.Allele;
 import de.ethasia.exorions.core.breeding.Chromosome;
 import de.ethasia.exorions.core.breeding.ChromosomePair;
 import de.ethasia.exorions.core.interfaces.CoreClassesFactory;
@@ -81,4 +82,145 @@ public class ChromosomePairTest {
         assertThat(clone.getMaternalChromosome(), is(not(equalTo(maternalChromosome))));
         assertThat(clone.getPaternalChromosome(), is(not(equalTo(paternalChromosome))));
     }
+    
+    @Test 
+    public void testCloneWithCrossover_resultingChromosomesHaveAllelesFromRng() throws NotAllPropertiesAreSetException {
+        RandomNumberGeneratorMock rngMock = (RandomNumberGeneratorMock)(new MockCoreClassesFactory().getRandomNumberGeneratorSingletonInstance());
+        rngMock.setBooleanSequenceToUse(new boolean[] {false, true, false, false, true, true, true, true, false, false, false, false, false, false, true, false, true, true, false, true});
+        rngMock.setIntegerSequenceToUse(new int[] {0, 0, -1, -1, 1, 0, 0, -1, -1, 0}); 
+        rngMock.reset();
+        
+        Chromosome maternalChromosome = new Chromosome.Builder().randomizeUndefinedAlleles().build();
+        Chromosome paternalChromosome = new Chromosome.Builder().randomizeUndefinedAlleles().build();
+        
+        ChromosomePair product = new ChromosomePair.Builder()
+            .setPaternalChromosome(paternalChromosome)
+            .setMaternalChromosome(maternalChromosome)
+            .build();
+        
+        ChromosomePair testCandidate = product.cloneWithCrossover();
+        ChromosomePair expected = getExpectedResultForCloneWithCrossoverTest();
+        
+        assertThat(testCandidate.getMaternalChromosome(), is(equalTo(expected.getMaternalChromosome())));
+        assertThat(testCandidate.getPaternalChromosome(), is(equalTo(expected.getPaternalChromosome())));
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="Helper Methods">
+    
+    private ChromosomePair getExpectedResultForCloneWithCrossoverTest() throws NotAllPropertiesAreSetException {        
+        Allele alleleMaxHealth = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(false)
+            .build();    
+        Allele alleleAttack = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(true)
+            .build();
+        Allele alleleDefense = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleSpecialAttack = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleSpecialDefense = new Allele.Builder()
+            .setStatModifier(1)
+            .setIsDominant(true)
+            .build();
+        Allele alleleSwiftness = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(true)
+            .build();
+        Allele alleleAccuracy = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(true)
+            .build();
+        Allele alleleEvasiveness = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(true)
+            .build();
+        Allele alleleCriticalHitAvoidance = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleCriticalHitFrequency = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(false)
+            .build();
+        
+        Allele alleleMaxHealthSecondChromosome = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(false)
+            .build();    
+        Allele alleleAttackSecondChromosome = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(false)
+            .build();
+        Allele alleleDefenseSecondChromosome = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleSpecialAttackSecondChromosome = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleSpecialDefenseSecondChromosome = new Allele.Builder()
+            .setStatModifier(1)
+            .setIsDominant(true)
+            .build();
+        Allele alleleSwiftnessSecondChromosome = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(false)
+            .build();
+        Allele alleleAccuracySecondChromosome = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(true)
+            .build();
+        Allele alleleEvasivenessSecondChromosome = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(true)
+            .build();
+        Allele alleleCriticalHitAvoidanceSecondChromosome = new Allele.Builder()
+            .setStatModifier(-1)
+            .setIsDominant(false)
+            .build();
+        Allele alleleCriticalHitFrequencySecondChromosome = new Allele.Builder()
+            .setStatModifier(0)
+            .setIsDominant(true)
+            .build();
+        
+        Chromosome firstExpectedChromosome = new Chromosome.Builder()
+            .setMaximumHealthAllele(alleleMaxHealthSecondChromosome)
+            .setAttackAllele(alleleAttack)
+            .setDefenseAllele(alleleDefenseSecondChromosome)
+            .setSpecialAttackAllele(alleleSpecialAttackSecondChromosome)
+            .setSpecialDefenseAllele(alleleSpecialDefense)
+            .setSwiftnessAllele(alleleSwiftness)
+            .setAccuracyAllele(alleleAccuracy)
+            .setEvasivenessAllele(alleleEvasiveness)
+            .setCriticalHitAvoidanceAllele(alleleCriticalHitAvoidanceSecondChromosome)
+            .setCriticalHitFrequencyAllele(alleleCriticalHitFrequencySecondChromosome)
+            .build();
+        
+        Chromosome secondExpectedChromosome = new Chromosome.Builder()
+            .setMaximumHealthAllele(alleleMaxHealth)
+            .setAttackAllele(alleleAttackSecondChromosome)
+            .setDefenseAllele(alleleDefense)
+            .setSpecialAttackAllele(alleleSpecialAttack)
+            .setSpecialDefenseAllele(alleleSpecialDefenseSecondChromosome)
+            .setSwiftnessAllele(alleleSwiftnessSecondChromosome)
+            .setAccuracyAllele(alleleAccuracySecondChromosome)
+            .setEvasivenessAllele(alleleEvasivenessSecondChromosome)
+            .setCriticalHitAvoidanceAllele(alleleCriticalHitAvoidance)
+            .setCriticalHitFrequencyAllele(alleleCriticalHitFrequency)
+            .build();
+        
+        return new ChromosomePair.Builder()
+            .setMaternalChromosome(firstExpectedChromosome)
+            .setPaternalChromosome(secondExpectedChromosome)
+            .build();
+    }
+    
+    //</editor-fold>
 }
