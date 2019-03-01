64f0efc801e21a882818ff34f24af99be6f3e91d
rondrejk <rondrejk@CE16848.corp.capgemini.com>
2019-03-01 19:35:44 +0100

    Added methods to calculate total gene values in ChromosomeSet.

--- src/de/ethasia/exorions/core/breeding/ChromosomeSet.java
+++ src/de/ethasia/exorions/core/breeding/ChromosomeSet.java
@@ -2,6 +2,325 @@ package de.ethasia.exorions.core.breeding;
 
 public class ChromosomeSet {
     
+    //<editor-fold defaultstate="collapsed" desc="Getters">
+    
+    private final ChromosomePair chromosomePairOne;
+    public ChromosomePair getChromosomePairOne() {
+        return chromosomePairOne;
+    }
+    
+    private final ChromosomePair chromosomePairTwo;
+    public ChromosomePair getChromosomePairTwo() {
+        return chromosomePairTwo;
+    }
+    
+    private final ChromosomePair chromosomePairThree;
+    public ChromosomePair getChromosomePairThree() {
+        return chromosomePairThree;
+    }
+    
+    private final ChromosomePair chromosomePairFour;
+    public ChromosomePair getChromosomePairFour() {
+        return chromosomePairFour;
+    }
+    
+    private final ChromosomePair chromosomePairFive;
+    public ChromosomePair getChromosomePairFive() {
+        return chromosomePairFive;
+    }
+
+    private final ChromosomePair chromosomePairSix;
+    public ChromosomePair getChromosomePairSix() {
+        return chromosomePairSix;
+    }
+    
+    private final ChromosomePair chromosomePairSeven;
+    public ChromosomePair getChromosomePairSeven() {
+        return chromosomePairSeven;
+    } 
+
+    private final ChromosomePair chromosomePairEight;
+    public ChromosomePair getChromosomePairEight() {
+        return chromosomePairEight;
+    }   
+    
+    private final ChromosomePair chromosomePairNine;
+    public ChromosomePair getChromosomePairNine() {
+        return chromosomePairNine;
+    }
+
+    private final ChromosomePair chromosomePairTen;
+    public ChromosomePair getChromosomePairTen() {
+        return chromosomePairTen;
+    } 
+    
+    private final ChromosomePair chromosomePairEleven;
+    public ChromosomePair getChromosomePairEleven() {
+        return chromosomePairEleven;
+    } 
+
+    private final ChromosomePair chromosomePairTwelve;
+    public ChromosomePair getChromosomePairTwelve() {
+        return chromosomePairTwelve;
+    } 
+
+    private final ChromosomePair chromosomePairThirteen;
+    public ChromosomePair getChromosomePairThirteen() {
+        return chromosomePairThirteen;
+    } 
+
+    private final ChromosomePair chromosomePairFourteen;
+    public ChromosomePair getChromosomePairFourteen() {
+        return chromosomePairFourteen;
+    } 
+
+    private final ChromosomePair chromosomePairFifteen;
+    public ChromosomePair getChromosomePairFifteen() {
+        return chromosomePairFifteen;
+    }
+    
+    private final ChromosomePair chromosomePairSixteen;
+    public ChromosomePair getChromosomePairSixteen() {
+        return chromosomePairSixteen;
+    } 
+
+    private final ChromosomePair chromosomePairSeventeen;
+    public ChromosomePair getChromosomePairSeventeen() {
+        return chromosomePairSeventeen;
+    } 
+
+    private final ChromosomePair chromosomePairEighteen;
+    public ChromosomePair getChromosomePairEighteen() {
+        return chromosomePairEighteen;
+    } 
+
+    private final ChromosomePair chromosomePairNineteen;
+    public ChromosomePair getChromosomePairNineteen() {
+        return chromosomePairNineteen;
+    } 
+
+    private final ChromosomePair chromosomePairTwenty;
+    public ChromosomePair getChromosomePairTwenty() {
+        return chromosomePairTwenty;
+    }     
+    
+    private final ChromosomePair chromosomePairTwentyone;
+    public ChromosomePair getChromosomePairTwentyone() {
+        return chromosomePairTwentyone;
+    } 
+
+    private final ChromosomePair chromosomePairTwentytwo;
+    public ChromosomePair getChromosomePairTwentytwo() {
+        return chromosomePairTwentytwo;
+    } 
+
+    private final ChromosomePair chromosomePairTwentythree;
+    public ChromosomePair getChromosomePairTwentythree() {
+        return chromosomePairTwentythree;
+    } 
+
+    private final ChromosomePair chromosomePairTwentyfour;
+    public ChromosomePair getChromosomePairTwentyfour() {
+        return chromosomePairTwentyfour;
+    } 
+
+    private final ChromosomePair chromosomePairTwentyfive;
+    public ChromosomePair getChromosomePairTwentyfive() {
+        return chromosomePairTwentyfive;
+    }     
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    private ChromosomeSet(Random builder) {
+        chromosomePairOne = builder.chromosomePairOne;
+        chromosomePairTwo = builder.chromosomePairTwo;
+        chromosomePairThree = builder.chromosomePairThree;
+        chromosomePairFour = builder.chromosomePairFour;
+        chromosomePairFive = builder.chromosomePairFive;
+        
+        chromosomePairSix = builder.chromosomePairSix;
+        chromosomePairSeven = builder.chromosomePairSeven;
+        chromosomePairEight = builder.chromosomePairEight;
+        chromosomePairNine = builder.chromosomePairNine;
+        chromosomePairTen = builder.chromosomePairTen;
+        
+        chromosomePairEleven = builder.chromosomePairEleven;
+        chromosomePairTwelve = builder.chromosomePairTwelve;
+        chromosomePairThirteen = builder.chromosomePairThirteen;
+        chromosomePairFourteen = builder.chromosomePairFourteen;
+        chromosomePairFifteen = builder.chromosomePairFifteen;
+
+        chromosomePairSixteen = builder.chromosomePairSixteen;
+        chromosomePairSeventeen = builder.chromosomePairSeventeen;
+        chromosomePairEighteen = builder.chromosomePairEighteen;
+        chromosomePairNineteen = builder.chromosomePairNineteen;
+        chromosomePairTwenty = builder.chromosomePairTwenty;  
+        
+        chromosomePairTwentyone = builder.chromosomePairTwentyone;
+        chromosomePairTwentytwo = builder.chromosomePairTwentytwo;
+        chromosomePairTwentythree = builder.chromosomePairTwentythree;
+        chromosomePairTwentyfour = builder.chromosomePairTwentyfour;
+        chromosomePairTwentyfive = builder.chromosomePairTwentyfive;         
+    }
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Methods">
+    
+    public int getMaximumHealthTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getMaximumHealthGeneValue();
+        }
+        
+        return result;
+    }
+    
+    public int getAttackTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getAttackGeneValue();
+        }
+        
+        return result;
+    }    
+    
+    public int getDefenseTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getDefenseGeneValue();
+        }
+        
+        return result;
+    }
+    
+    public int getSpecialAttackTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getSpecialAttackGeneValue();
+        }
+        
+        return result;
+    }
+    
+    public int getSpecialDefenseTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getSpecialDefenseGeneValue();
+        }
+        
+        return result;
+    }   
+    
+    public int getAccuracyTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getAccuracyGeneValue();
+        }
+        
+        return result;
+    }
+
+    public int getEvasivenessTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getEvasivenessGeneValue();
+        }
+        
+        return result;
+    }  
+    
+    public int getCriticalHitFrequencyTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getCriticalHitFrequencyGeneValue();
+        }
+        
+        return result;
+    } 
+
+    public int getCriticalHitAvoidanceTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getCriticalHitAvoidanceGeneValue();
+        }
+        
+        return result;
+    }    
+    
+    public int getSwiftnessTotalGeneValue() {
+        int result = 0;
+        
+        ChromosomePair[] chromosomePairs = getAllChromosomePairs();
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            result += chromosomePair.getSwiftnessGeneValue();
+        }
+        
+        return result;
+    }    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Helper Methods">
+    
+    private ChromosomePair[] getAllChromosomePairs() {
+        ChromosomePair[] result = new ChromosomePair[25];
+        
+        result[0] = getChromosomePairOne();
+        result[1] = getChromosomePairTwo();
+        result[2] = getChromosomePairThree();
+        result[3] = getChromosomePairFour();
+        result[4] = getChromosomePairFive();
+        
+        result[5] = getChromosomePairSix();
+        result[6] = getChromosomePairSeven();
+        result[7] = getChromosomePairEight();
+        result[8] = getChromosomePairNine();
+        result[9] = getChromosomePairTen();  
+        
+        result[10] = getChromosomePairEleven();
+        result[11] = getChromosomePairTwelve();
+        result[12] = getChromosomePairThirteen();
+        result[13] = getChromosomePairFourteen();
+        result[14] = getChromosomePairFifteen();  
+        
+        result[15] = getChromosomePairSixteen();
+        result[16] = getChromosomePairSeventeen();
+        result[17] = getChromosomePairEighteen();
+        result[18] = getChromosomePairNineteen();
+        result[19] = getChromosomePairTwenty();   
+        
+        result[20] = getChromosomePairTwentyone();
+        result[21] = getChromosomePairTwentytwo();
+        result[22] = getChromosomePairTwentythree();
+        result[23] = getChromosomePairTwentyfour();
+        result[24] = getChromosomePairTwentyfive();         
+        
+        return result;
+    }    
+    
+    //</editor-fold>
+    
     //<editor-fold defaultstate="collapsed" desc="Random Builder">
     
     public static class Random {
@@ -67,7 +386,7 @@ public class ChromosomeSet {
             chromosomePairTwentyfour = new ChromosomePair.Random().build();
             chromosomePairTwentyfive = new ChromosomePair.Random().build();
             
-            return new ChromosomeSet();
+            return new ChromosomeSet(this);
         }
     }
     
--- tests/de/ethasia/exorions/core/breeding/tests/ChromosomeSetTest.java
+++ tests/de/ethasia/exorions/core/breeding/tests/ChromosomeSetTest.java
@@ -1,5 +1,6 @@
 package de.ethasia.exorions.core.breeding.tests;
 
+import de.ethasia.exorions.core.breeding.ChromosomePair;
 import de.ethasia.exorions.core.breeding.ChromosomeSet;
 import de.ethasia.exorions.core.interfaces.CoreClassesFactory;
 import de.ethasia.exorions.core.mocks.MockCoreClassesFactory;
@@ -7,6 +8,7 @@ import de.ethasia.exorions.core.mocks.RandomNumberGeneratorMock;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,15 +17,20 @@ public class ChromosomeSetTest {
     @BeforeClass
     public static void initDependencies() {
         CoreClassesFactory.setInstance(new MockCoreClassesFactory());
+        RandomNumberGeneratorMock rngMock = (RandomNumberGeneratorMock)(new MockCoreClassesFactory().getRandomNumberGeneratorSingletonInstance());
+        rngMock.setBooleanSequenceToUse(new boolean[] {false, true, false, false, true, true, true, true, false, false, false, false, false, false, true, false, true, true, false, true});
+        rngMock.setIntegerSequenceToUse(new int[] {1, -1, 1, -1, 1, 1, 0, -1, -1, 1, 0, -1, 1, -1, 1, 0, -1, -1, -1}); 
+    }
+    
+    @Before
+    public void prepareUnitTests() {
+        RandomNumberGeneratorMock rngMock = (RandomNumberGeneratorMock)(new MockCoreClassesFactory().getRandomNumberGeneratorSingletonInstance());
+        rngMock.reset();         
     }
 
     @Test
-    public void testRandomBuilder_createsRandomChromosomeSetByCallingRng() {
+    public void testRandomBuilder_createsRandomChromosomeSetByCallingRng() {   
         RandomNumberGeneratorMock rngMock = (RandomNumberGeneratorMock)(new MockCoreClassesFactory().getRandomNumberGeneratorSingletonInstance());
-        rngMock.setBooleanSequenceToUse(new boolean[] {false, true, false, false, true, true, true, true, false, false, false, false, false, false, true, false, true, true, false, true});
-        rngMock.setIntegerSequenceToUse(new int[] {0, 0, -1, -1, 1, 0, 0, -1, -1, 0});        
-        rngMock.reset();        
-        
         ChromosomeSet product = new ChromosomeSet.Random().build();
         
         int allelesPerChromosomePair = 20;
@@ -34,4 +41,229 @@ public class ChromosomeSetTest {
         assertThat(rngMock.getCallCount("createRandomIntegerBetweenAnd"), is(expectedRngCallCount));
         assertThat(rngMock.getCallCount("createRandomBoolean"), is(expectedRngCallCount));        
     }
+    
+    @Test
+    public void testGetChromosomePair_fromRandomlyCreatedSet_pairsAreReturned() {
+        ChromosomeSet product = new ChromosomeSet.Random().build();
+        
+        assertThat(product.getChromosomePairOne(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwo(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairThree(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairFour(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairFive(), is(ChromosomePair.class));
+        
+        assertThat(product.getChromosomePairSix(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairSeven(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairEight(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairNine(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTen(), is(ChromosomePair.class));
+        
+        assertThat(product.getChromosomePairEleven(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwelve(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairThirteen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairFourteen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairFifteen(), is(ChromosomePair.class));  
+        
+        assertThat(product.getChromosomePairSixteen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairSeventeen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairEighteen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairNineteen(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwenty(), is(ChromosomePair.class));         
+        
+        assertThat(product.getChromosomePairTwentyone(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwentytwo(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwentythree(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwentyfour(), is(ChromosomePair.class));
+        assertThat(product.getChromosomePairTwentyfive(), is(ChromosomePair.class)); 
+    }
+    
+    @Test
+    public void testGetMaximumHealthTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getMaximumHealthTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getMaximumHealthGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    public void testGetAttackTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getAttackTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getAttackGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }  
+    
+    @Test
+    public void testGetDefenseTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getDefenseTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getDefenseGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    } 
+
+    @Test
+    public void testGetSpecialAttackTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getSpecialAttackTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getSpecialAttackGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }  
+    
+    @Test
+    public void testGetSpecialDefenseTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getSpecialDefenseTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getSpecialDefenseGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }    
+    
+    @Test
+    public void testGetAccuracyTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getAccuracyTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getAccuracyGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    } 
+    
+    @Test
+    public void testGetEvasivenessTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getEvasivenessTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getEvasivenessGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }  
+    
+    @Test
+    public void testGetCriticalHitFrequencyTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getCriticalHitFrequencyTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getCriticalHitFrequencyGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }  
+    
+    @Test
+    public void testGetCriticalHitAvoidanceTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getCriticalHitAvoidanceTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getCriticalHitAvoidanceGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    public void testGetSwiftnessTotalGeneValue_isEqualToSumOfGeneValuesOfChromosomePairs() {
+        ChromosomeSet testCandidate = new ChromosomeSet.Random().build();
+        
+        int actual = testCandidate.getSwiftnessTotalGeneValue();
+        ChromosomePair[] chromosomePairs = getAllChromosomePairsFromChromosomeSet(testCandidate);
+        
+        int expected = 0;
+        for (ChromosomePair chromosomePair : chromosomePairs) {
+            expected += chromosomePair.getSwiftnessGeneValue();
+        }
+        
+        assertThat(actual, is(equalTo(expected)));
+    }     
+    
+    //<editor-fold defaultstate="collapsed" desc="Helper Methods">
+    
+    private ChromosomePair[] getAllChromosomePairsFromChromosomeSet(ChromosomeSet source) {
+        ChromosomePair[] result = new ChromosomePair[25];
+        
+        result[0] = source.getChromosomePairOne();
+        result[1] = source.getChromosomePairTwo();
+        result[2] = source.getChromosomePairThree();
+        result[3] = source.getChromosomePairFour();
+        result[4] = source.getChromosomePairFive();
+        
+        result[5] = source.getChromosomePairSix();
+        result[6] = source.getChromosomePairSeven();
+        result[7] = source.getChromosomePairEight();
+        result[8] = source.getChromosomePairNine();
+        result[9] = source.getChromosomePairTen();  
+        
+        result[10] = source.getChromosomePairEleven();
+        result[11] = source.getChromosomePairTwelve();
+        result[12] = source.getChromosomePairThirteen();
+        result[13] = source.getChromosomePairFourteen();
+        result[14] = source.getChromosomePairFifteen();  
+        
+        result[15] = source.getChromosomePairSixteen();
+        result[16] = source.getChromosomePairSeventeen();
+        result[17] = source.getChromosomePairEighteen();
+        result[18] = source.getChromosomePairNineteen();
+        result[19] = source.getChromosomePairTwenty();   
+        
+        result[20] = source.getChromosomePairTwentyone();
+        result[21] = source.getChromosomePairTwentytwo();
+        result[22] = source.getChromosomePairTwentythree();
+        result[23] = source.getChromosomePairTwentyfour();
+        result[24] = source.getChromosomePairTwentyfive();         
+        
+        return result;
+    }
+    
+    //</editor-fold>
 }
