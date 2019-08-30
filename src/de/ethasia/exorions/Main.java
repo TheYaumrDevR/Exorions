af8400a0e6f50d19b25f1e0415d63fd5f88a0b99
drawi <drawi@LAPTOP-6LMVMR3M.fritz.box>
2019-08-31 01:18:03 +0200

    Added computer model.

--- src/de/ethasia/exorions/Main.java
+++ src/de/ethasia/exorions/Main.java
@@ -1,7 +1,10 @@
 package de.ethasia.exorions;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Spatial;
 
 public class Main extends SimpleApplication {
 
@@ -14,7 +17,12 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        Spatial teapot = assetManager.loadModel("Models/Player Room/Computer/Computer.j3o");
+        rootNode.attachChild(teapot);        
         
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+        rootNode.addLight(sun);
     }
 
     @Override
