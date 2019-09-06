9fe2874cd68b4ceb5200c1c680c3c461c15d7317
rondrejk <rondrejk@CE16848.corp.capgemini.com>
2019-09-06 17:54:03 +0200

    Dialogs second step: DialogWithOptions implemented.

--- src/de/ethasia/exorions/core/dialogs/DialogWithOptions.java
+++ src/de/ethasia/exorions/core/dialogs/DialogWithOptions.java
@@ -1,22 +1,31 @@
 package de.ethasia.exorions.core.dialogs;
 
+import de.ethasia.exorions.core.general.NotAllPropertiesAreSetException;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class DialogWithOptions implements DialogNode {
     
-    //<editor-fold defaultstate="collapsed" desc="Builder">
+    //<editor-fold defaultstate="collapsed" desc="Properties">
     
-    public static class Builder {
-        
-        public Builder setText(String value) {
-            return this;
-        }
-        
-        public Builder addDialogOption(DialogOption value) {
-            return this;
-        }        
-        
-        public DialogWithOptions build() {
-            return new DialogWithOptions();
-        }
+    private final String text;
+    public String getText() {
+        return text;
+    }
+    
+    private final List<DialogOption> options;
+    public List<DialogOption> getOptions() {
+        return options;
+    }
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    private DialogWithOptions(Builder builder) {
+        text = builder.text;
+        options = builder.dialogOptions;
     }
     
     //</editor-fold>
@@ -27,6 +36,38 @@ public class DialogWithOptions implements DialogNode {
     public boolean isLeaf() {
         return false;
     }    
+    
+    //</editor-fold>    
+    
+    //<editor-fold defaultstate="collapsed" desc="Builder">
+    
+    public static class Builder {
+        
+        private String text;
+        private final List<DialogOption> dialogOptions;
+        
+        public Builder() {
+            dialogOptions = new LinkedList<>();
+        }
+        
+        public Builder setText(String value) {
+            text = value;
+            return this;
+        }
+        
+        public Builder addDialogOption(DialogOption value) {
+            dialogOptions.add(value);
+            return this;
+        }        
+        
+        public DialogWithOptions build() {
+            if (dialogOptions.size() < 2) {
+                throw new NotAllPropertiesAreSetException();
+            }
+            
+            return new DialogWithOptions(this);
+        }
+    }
     
     //</editor-fold>
 }
--- tests/de/ethasia/exorions/core/dialogs/DialogWithOptionsTest.java
+++ tests/de/ethasia/exorions/core/dialogs/DialogWithOptionsTest.java
@@ -1,29 +1,100 @@
 package de.ethasia.exorions.core.dialogs;
 
 import de.ethasia.exorions.core.general.NotAllPropertiesAreSetException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DialogWithOptionsTest {
 
     @Test
-    public void testBuilder_setAllProperties_builds() {
+    public void testBuilder_setAllProperties_productContainsAllProperties() {
+        DialogEndnode endnode = new DialogEndnode("Make your time.");
         
+        DialogOption followUpOptionOne = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About the Planet")
+            .build(); 
+        
+        DialogOption followUpOptionTwo = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About Controllers")
+            .build();          
+        
+        DialogWithOptions testCandidate = new DialogWithOptions.Builder()
+            .setText("I really don't feel like talking.")
+            .addDialogOption(followUpOptionOne)
+            .addDialogOption(followUpOptionTwo)
+            .build();  
+        
+        assertThat(testCandidate.getText(), is(equalTo("I really don't feel like talking.")));
+        assertThat(testCandidate.getOptions().size(), is(equalTo(2)));
+        assertThat(testCandidate.getOptions(), hasItems(followUpOptionOne, followUpOptionTwo));
     }
     
     @Test
     public void testThatItImplementsDialogNode() {
+        DialogEndnode endnode = new DialogEndnode("Make your time.");
         
+        DialogOption followUpOptionOne = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About the Planet")
+            .build(); 
+        
+        DialogOption followUpOptionTwo = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About Controllers")
+            .build();          
+        
+        DialogWithOptions testCandidate = new DialogWithOptions.Builder()
+            .setText("I really don't feel like talking.")
+            .addDialogOption(followUpOptionOne)
+            .addDialogOption(followUpOptionTwo)
+            .build();
+        
+        boolean productImplementsDialogNode = testCandidate instanceof DialogNode;
+        
+        assertThat(productImplementsDialogNode, is(true));
     }
     
     @Test
     public void testIsLeaf_returnsFalse() {
+        DialogEndnode endnode = new DialogEndnode("Make your time.");
         
+        DialogOption followUpOptionOne = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About the Planet")
+            .build(); 
+        
+        DialogOption followUpOptionTwo = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About Controllers")
+            .build();          
+        
+        DialogWithOptions testCandidate = new DialogWithOptions.Builder()
+            .setText("I really don't feel like talking.")
+            .addDialogOption(followUpOptionOne)
+            .addDialogOption(followUpOptionTwo)
+            .build();
+        
+        assertThat(testCandidate.isLeaf(), is(false));
     }
     
-    // @Test(expected = NotAllPropertiesAreSetException.class)
+    @Test(expected = NotAllPropertiesAreSetException.class)
     public void testBuilder_lessThanTwoOptionsAreGiven_throwsException() {
+        DialogEndnode endnode = new DialogEndnode("Make your time.");
         
+        DialogOption followUpOptionOne = new DialogOption.Builder()
+            .setFollowUpNode(endnode)
+            .setText("About the Planet")
+            .build();         
+        
+        DialogWithOptions followUpNode = new DialogWithOptions.Builder()
+            .setText("I really don't feel like talking.")
+            .addDialogOption(followUpOptionOne)
+            .build();        
     }
 }
