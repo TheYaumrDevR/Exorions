19fac890310692d8b5008da01092ce1113c00e18
Rastislav Ondrejka <rastislav.ondrejka@capgemini.com>
2019-11-07 20:55:09 +0100

    Extended the MapDefinitionsGatewayMock to retrieve parameter values for last mocked method calls.

--- tests/de/ethasia/exorions/interactors/mocks/MapDefinitionsGatewayMock.java
+++ tests/de/ethasia/exorions/interactors/mocks/MapDefinitionsGatewayMock.java
@@ -9,13 +9,40 @@ public class MapDefinitionsGatewayMock implements MapDefinitionsGateway {
     //<editor-fold defaultstate="collapsed" desc="Static Fields">
     
     private static RuntimeException nextExceptionToThrow;
+    private static String lastPassedFindFloorTileDefinitionsPath;
+    private static String lastPassedFindCollisionTileDefinitionsPath;
+    private static String lastPassedGetMapDimensionXPath;
+    private static String lastPassedGetMapDimensionZPath;
     
     //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="Static Methods">
     
+    public static void resetMocks() {
+        lastPassedFindFloorTileDefinitionsPath = "";
+        lastPassedFindCollisionTileDefinitionsPath = "";
+        lastPassedGetMapDimensionXPath = "";
+        lastPassedGetMapDimensionZPath = "";
+    }
+    
     public static void setNextExceptionToThrow(RuntimeException value) {
         nextExceptionToThrow = value;
+    }
+    
+    public static String getLastPassedFindFloorTileDefinitionsPath() {
+        return lastPassedFindFloorTileDefinitionsPath;
+    }
+    
+    public static String getLastPassedFindCollisionTileDefinitionsPath() {
+        return lastPassedFindCollisionTileDefinitionsPath;
+    }
+    
+    public static String getLastPassedGetMapDimensionXPath() {
+        return lastPassedGetMapDimensionXPath;
+    }
+    
+    public static String getLastPassedGetMapDimensionZPath() {
+        return lastPassedGetMapDimensionZPath;
     }
     
     //</editor-fold>
@@ -25,30 +52,34 @@ public class MapDefinitionsGatewayMock implements MapDefinitionsGateway {
     @Override
     public int getMapDimensionX(String pathToMapDefinition) {
         throwExceptionIfSet();
+        lastPassedGetMapDimensionXPath = pathToMapDefinition;
         return 0;
     }
 
     @Override
     public int getMapDimensionZ(String pathToMapDefinition) {
         throwExceptionIfSet();
+        lastPassedGetMapDimensionZPath = pathToMapDefinition;
         return 0;
     }    
     
     @Override
     public MapMetaData tryToRetrieveMetaDataForNewGameMap() {
         throwExceptionIfSet();
-        return new MapMetaData("", "", "");
+        return new MapMetaData("Suriver City NE", "Maps/Exterior/SuriverCityNE.xml", "Scenes/Exterior/SuriverCityNE.j3o");
     }    
     
     @Override
     public DefinitionsForUndistinguishableMapTiles findFloorTileDefinitions(String pathToMapDefinition) {
         throwExceptionIfSet();
+        lastPassedFindFloorTileDefinitionsPath = pathToMapDefinition;
         return new DefinitionsForUndistinguishableMapTiles();
     }  
     
     @Override
     public DefinitionsForUndistinguishableMapTiles findCollisionTileDefinitions(String pathToMapDefinition) {
         throwExceptionIfSet();
+        lastPassedFindCollisionTileDefinitionsPath = pathToMapDefinition;
         return new DefinitionsForUndistinguishableMapTiles();
     }    
     
